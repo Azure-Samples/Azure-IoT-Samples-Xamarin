0b91dd230a74004c26864e504a7035e3cf68c79a
Christopher Maneu <christopher@maneu.net>
2020-06-11 17:22:48 +0200

    Import project

---
+++ iot-hub/quickstart/simulated-device/SimulatedDevice/DeviceReadingMessage.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SimulatedDevice
+{
+    /// <summary>
+    /// This class represents the message serialized and sent to IoT Hub
+    /// </summary>
+    class DeviceReadingMessage
+    {
+        [JsonPropertyName("t")]
+        public string Temperature { get; set; }
+        [JsonPropertyName("h")]
+        public string Humidity { get; set; }
+    }
+}
